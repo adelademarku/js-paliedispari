d4a205936786b8513172d2789119a418f263214f
adelademarku <adelademarku@email.com>
2022-11-16 14:54:33 +0100

    sistemato i commenti

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+//chiedo al utente di inserire una parola
 
 let parolaInserita = prompt ("Inserisci una parola!");
 
+//stampo la funzione
+
 let parolaInseritaInvertita = stringaInvertita (parolaInserita);
+
+//verifico se la parola inserita dal utente è palindroma
 
 if (parolaInserita == parolaInseritaInvertita){
     console.log ("la parola inserita è palindroma")
 }
 
+
+
+// creo la funzione dove posso invertire una stringa
 
 function stringaInvertita (stringa) {
 
